5bcf8d6d2de0e3c0c2575e2679cbaadba9dae8da
gencery <genceryigiter@gmail.com>
2024-02-06 14:13:57 +0300

    .

--- routes/altSektorler.js
+++ routes/altSektorler.js
@@ -3,63 +3,67 @@ import express from "express";
 import db from "../conn.js";
 
 const router = express.Router();
-//READ ALL SECTORS (OR add a condition)
-// router.get("/altSektorler/list/:sektorId", async (req, res) => {
+//READ ALL SUBSECTORS
+router.get("/altSektorler/list/:sektorId", async (req, res) => {
 
-// 	let sektorId = req.params.sektorId;
+   let sektorId = req.params.sektorId;
+   let result = db
+      .collection("altSektorler")
 
-// 	const result = await db
-// 		.collection("altSektorler")
-// 		.aggregate(
-// 			[
-// 				{ $sort: { name: 1 } },
-// 				{
-// 					$cond: {if : {$}
-// 					$match: {
-// 						sektorId: sektorId
-// 					}
-// 				},
-// 			],
-// 			{ collation: { locale: "tr" } }
-// 		)
-// 		.toArray();
-// 	//.find({ age: { $gte: 25 } })
-// 	//.toArray();
+   if (sektorId == "all") {
+      result = await result.aggregate(
+         [
+            { $sort: { name: 1 } },
+         ],
+         { collation: { locale: "tr" } }
+      )
+         .toArray();
+   }
+   else {
+      result = await result.aggregate(
+         [
+            { $sort: { name: 1 } },
+            { $match: { sektorId: sektorId } },
+         ],
+         { collation: { locale: "tr" } }
+      )
+         .toArray();
+   }
 
-// 	res.json({
-// 		count: result.length,
-// 		result: result,
-// 	});
-// });
+   res.json({
+      count: result.length,
+      result: result,
+   });
+});
 
 
 
-// //ADD A SECTOR
-// router.get("/sektorler/add/:name/", async (req, res) => {
-// 	const result = await db
-// 		.collection("sektorler")
-// 		.insertOne({ name: req.params.name });
-// 	res.json(result);
-// });
+//ADD A SUBSECTOR
+router.get("/altSektorler/add/:sektorId/:name", async (req, res) => {
+   const result = await db
+      .collection("altSektorler")
+      .insertOne({ name: req.params.name, sektorId: req.params.sektorId });
+   res.json(result);
+});
 
-// //DELETE A SECTOR
-// router.get("/sektorler/delete/:id", async (req, res) => {
-// 	const result = await db
-// 		.collection("sektorler")
-// 		.deleteOne({ _id: new mongodb.ObjectId(req.params.id) });
-// 	res.json(result);
-// });
+//DELETE A SUBSECTOR
+router.get("/altSektorler/delete/:id", async (req, res) => {
+   const result = await db
+      .collection("altSektorler")
+      .deleteOne({ _id: new mongodb.ObjectId(req.params.id) });
+   res.json(result);
+});
 
-// //UPDATE A SECTOR
-// router.get("/sektorler/update/:id/:name/", async (req, res) => {
-// 	const result = await db
-// 		.collection("sektorler")
-// 		.replaceOne(
-// 			{ _id: new mongodb.ObjectId(req.params.id) },
-// 			{ name: req.params.name }
-// 		);
+// //UPDATE A SUBSECTOR
+router.get("/altSektorler/update/:id/:sektorId/:name", async (req, res) => {
+   const result = await db
+      .collection("sektorler")
+      .replaceOne(
+         { _id: new mongodb.ObjectId(req.params.id) },
+         { name: req.params.name, sektorId: req.params.sektorId }
+      );
 
-// 	res.json(result);
-// });
+   res.json(result);
+});
 
 export default router;
